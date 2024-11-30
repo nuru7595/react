488255c6f2c10a2ee68e9115c216bca7ec4d9fc0
Nuru <nuruzzamannuru7595@gmail.com>
2024-11-30 21:40:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      ubuntu: ["Ubuntu", "sans-serif"]
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: [],
+}
+
